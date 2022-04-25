051200ee7a1a0be692471b721ef84592f9b35d5e
Sebastian Naranjo <sebas2013n@gmail.com>
2022-04-25 16:56:12 -0500

    Inicio

--- FrontendAngularPeliculas/src/app/app.module.ts
+++ FrontendAngularPeliculas/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
