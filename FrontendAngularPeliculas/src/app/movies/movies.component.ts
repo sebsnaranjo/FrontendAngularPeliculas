25ae5daf39804124d843f5d535026f67486c8fa9
Sebastian Naranjo <sebas2013n@gmail.com>
2022-04-27 08:04:50 -0500

    Ajustes finales

--- FrontendAngularPeliculas/src/app/movies/movies.component.ts
+++ FrontendAngularPeliculas/src/app/movies/movies.component.ts
@@ -20,14 +20,24 @@ export class MoviesComponent implements OnInit {
   pelicula_8!: Movies
   pelicula_9!: Movies
   pelicula_10!: Movies
+  pelicula_11!: Movies
+  pelicula_12!: Movies
+  pelicula_13!: Movies
+  pelicula_14!: Movies
+  pelicula_15!: Movies
+  pelicula_16!: Movies
+  pelicula_17!: Movies
+  pelicula_18!: Movies
+  pelicula_19!: Movies
+  pelicula_20!: Movies
 
   constructor(private movie:MovieService) { }
 
   ngOnInit(): void { }
 
   search(){
-    console.log("entro")
     this.movie.getMovie(this.title).subscribe(data => {
+      console.log(data)
       this.pelicula_1 = data[0]["Pelicula"]
       this.pelicula_2 = data[1]["Pelicula"]
       this.pelicula_3 = data[2]["Pelicula"]
@@ -38,7 +48,22 @@ export class MoviesComponent implements OnInit {
       this.pelicula_8 = data[7]["Pelicula"]
       this.pelicula_9 = data[8]["Pelicula"]
       this.pelicula_10 = data[9]["Pelicula"]
-      console.log(this.pelicula_1)
+    })
+  }
+
+  search2(){
+    this.movie.getMovieM(this.title).subscribe(info => {
+      console.log(info)
+      this.pelicula_11 = info[0]["Pelicula"]
+      this.pelicula_12 = info[1]["Pelicula"]
+      this.pelicula_13 = info[2]["Pelicula"]
+      this.pelicula_14 = info[3]["Pelicula"]
+      this.pelicula_15 = info[4]["Pelicula"]
+      this.pelicula_16 = info[5]["Pelicula"]
+      this.pelicula_17 = info[6]["Pelicula"]
+      this.pelicula_18 = info[7]["Pelicula"]
+      this.pelicula_19 = info[8]["Pelicula"]
+      this.pelicula_20 = info[9]["Pelicula"]
     })
   }
 
--- FrontendAngularPeliculas/src/app/services/movie.service.ts
+++ FrontendAngularPeliculas/src/app/services/movie.service.ts
@@ -7,12 +7,17 @@ import { Movies } from '../modelos/movie.interface';
 })
 export class MovieService {
 
-  private url: string = 'http://127.0.0.1:8000/?title='
+  private url: string = 'http://127.0.0.1:8000/c?title='
+  private url2: string = 'http://127.0.0.1:8000/m?title='
 
   constructor(private http:HttpClient) { }
 
   getMovie(title: string){
     return this.http.get<any>(this.url + title)
+  }
+
+  getMovieM(title: string){
+    return this.http.get<any>(this.url2 + title)
   }
 
 }
